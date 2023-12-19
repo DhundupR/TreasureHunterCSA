2a2b235373bb9c03f822b424f02ed37bf6b7497c
BT_1N3_28 <bths.edu@samplwe>
2023-12-19 09:21:19 -0500

    commit Test

--- src/TreasureHunter.java
+++ src/TreasureHunter.java
@@ -54,7 +54,7 @@ public class TreasureHunter {
             hardMode = true;
         }
         else if(hard.equals("test")){
-            hunter.changeGold(7);
+            hunter.changeGold(0);
             hunter.buyItem("water",1);
             hunter.buyItem("rope",1);
             hunter.buyItem("machete",1);
